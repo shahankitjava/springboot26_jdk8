f13151f0592fa07e13c7b874412b842afd5d1c89
shahankitjava <shahankit.java@gmail.com>
2024-09-06 22:59:59 -0500

    Example commit

--- src/main/java/com/openrewrite/springboot26_jdk8/Springboot26Jdk8Application.java
+++ src/main/java/com/openrewrite/springboot26_jdk8/Springboot26Jdk8Application.java
@@ -8,6 +8,7 @@ public class Springboot26Jdk8Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot26Jdk8Application.class, args);
+		System.out.println("Application Started");
 	}
 
 }
